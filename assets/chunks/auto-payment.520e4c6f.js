ed31fd55b2ea51ca9ad8c8ec1c6448ea30867a88
18alantom <2.alan.tom@gmail.com>
2023-08-22 11:57:33 +0530

    deploy

---
+++ assets/chunks/auto-payment.520e4c6f.js
@@ -0,0 +1,1 @@
+var a="/assets/payment-accounts.2e45a03d.png",s="/assets/auto-payment.16a5469b.png";export{a as _,s as a};
--- assets/chunks/auto-payment.9329cc7b.js
+++
@@ -1,1 +0,0 @@
-var a="/assets/payment-accounts.a274c5ab.png",s="/assets/auto-payment.3872b1cf.png";export{a as _,s as a};
